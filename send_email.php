3c0b9d3fa12eb97cf0b859063360323cea1e2ed6
Rupinder <rupinder.developer@gmail.com>
2020-04-29 18:26:08 +0530

    Remove pass

--- send_email.php
+++ send_email.php
@@ -19,7 +19,7 @@ try {
     $mail->Host       = 'mail.rupindersingh.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'me@rupindersingh.com';                     // SMTP username
-    $mail->Password   = 'Developer101999';                               // SMTP password
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
